8147bb98efd8d841c1bd508d4376d4030efe9273
kyle-n <kylebnazario@gmail.com>
2020-12-19 16:01:55 -0700

    More home layouts

--- src/Home.tsx
+++ src/Home.tsx
@@ -11,12 +11,20 @@ const HomePage = () => (
       <Title level={1}>Fantastical Enterprise</Title>
     </Header>
     <Content id="splash-content">
-      <div>
+      <div className="solo-text">
         <img src={hero} alt="Fantastical running on Mac, iPad, iPhone and Apple Watch" />
       </div>
-      <div>
+      <div className="solo-text">
         <Title level={2}>Powerful features for power users.</Title>
         <Title level={3} type="secondary">Help your team work faster with the best calendar and tasks app.</Title>
+      </div>
+      <div id="splash-paragraph-container">
+        <Paragraph>
+          Fantastical is packed with amazing features, like finding meeting times that work for everyone.
+        </Paragraph>
+        <Paragraph>
+          Sign your whole team up instantly. No waiting, no sales teams, just instant productivity. 
+        </Paragraph>
       </div>
     </Content>
   </Layout>
