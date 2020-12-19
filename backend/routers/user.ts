cb70c5f9ce8bb0f98c8d21e7c9d8067340703b57
kyle-n <kylebnazario@gmail.com>
2020-12-19 12:41:02 -0700

    POST user

--- backend/routers/user.ts
+++ backend/routers/user.ts
@@ -1,5 +1,22 @@
 import {Router} from 'express';
+import { UserData } from '../../models/user';
 
 const router = Router();
+
+router.post('/', async (req, resp) => {
+  try {
+    await global.db.query({
+      sql: `INSERT INTO users (email, password, companyId, active, lastSignInDate, signUpDate) VALUES (:email, :password, :companyId, :active, :lastSignInDate, :signUpDate);`,
+      params: req.body
+    });
+    const {results} = await global.db.query({
+      sql: `SELECT * FROM users WHERE id = LAST_INSERT_ID();`,
+    });
+    const newUserData: UserData = results[0];
+    return resp.status(200).json(newUserData);
+  } catch (e) {
+    return resp.status(500).send(e);
+  }
+});
 
 export default router;
