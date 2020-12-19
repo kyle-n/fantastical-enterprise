e1a276e4939c98e7b54025abc30ceefebef470cc
kyle-n <kylebnazario@gmail.com>
2020-12-18 19:37:29 -0700

    Converts to TS type

--- backend/fantastical-server.ts
+++ backend/fantastical-server.ts
@@ -1,12 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
